56f6fa3f0fe5ca561e764dae0e3eff09d8bb9853
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-04-25 16:26:32 -0400

    Refactorisation Todo + méthodes CRUD asynchrones API
    Suppression du champ Body et ajout du booléen Completed dans le modèle Todo pour coller à la structure de l'API JSONPlaceholder. Ajout dans RestApiQueries.cs de méthodes asynchrones pour gérer les opérations CRUD sur les todos (GetTodosAsync, GetTodoAsync, AddTodoAsync, CompleteTodoAsync, RemoveTodoAsync) avec sérialisation JSON via Newtonsoft.Json. Ces changements modernisent l'accès aux données et améliorent la compatibilité avec l'API RESTful.

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -9,7 +9,7 @@ namespace HttpClientSample.Models
         public int UserId { get; set; }
         public int Id { get; set; }
         public string Title { get; set; }
-        public decimal Body { get; set; }
+        public bool Completed { get; set; } = false;
 
     }
 }
--- RestApiQueries.cs
+++ RestApiQueries.cs
@@ -3,8 +3,10 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text;
 using System.Threading.Tasks;
 using HttpClientSample.Models;
+using Newtonsoft.Json;
 
 
 namespace HttpClientSample
@@ -20,6 +22,113 @@ namespace HttpClientSample
             _client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
             _client.DefaultRequestHeaders.Accept.Clear();
             _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        // ======= ALL THE ASYNC FUNCTIONS ========
+
+        /// <summary>
+        /// Async function to get all the todos
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public async Task<List<Todo>> GetTodosAsync(string path)
+        {
+            HttpResponseMessage response = await _client.GetAsync(path);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+
+                List<Todo> todos = JsonConvert.DeserializeObject<List<Todo>>(data);
+
+                return todos;
+            }
+
+            return new List<Todo>();
+        }
+
+        /// <summary>
+        ///  Async function to get a specifique todo
+        /// </summary>
+        /// <param name="todoId"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public async Task<Todo> GetTodoAsync(int todoId, string path)
+        {
+
+            HttpResponseMessage response = await _client.GetAsync($"{path}/{todoId}");
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                string data = await response.Content.ReadAsStringAsync();
+                Todo todo = JsonConvert.DeserializeObject<Todo>(data);
+                return todo;
+            }
+
+            return new Todo();
+        }
+
+        /// <summary>
+        /// Async function to create a todo
+        /// </summary>
+        /// <param name="todo"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public async Task<bool> AddTodoAsync(Todo todo, string path)
+        {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(todo), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _client.PostAsync(path, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Async function to modify a specific todo
+        /// </summary>
+        /// <param name="todo"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public async Task<bool> CompleteTodoAsync(Todo todo, string path)
+        {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(todo), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _client.PutAsync($"{path}/{todo.Id}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Async function to delete s specifique todo
+        /// </summary>
+        /// <param name="todoId"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public async Task<bool> RemoveTodoAsync(int todoId, string path)
+        {
+            HttpResponseMessage response = await _client.DeleteAsync($"{path}/{todoId}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 
