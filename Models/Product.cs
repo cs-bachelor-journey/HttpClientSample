917a44fccb5851e8b06a743808271d557e88a5ce
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-04-25 15:38:28 -0400

    feat: Ajout des bases pour requêtes HTTP et modèle Product
    - Ajout des packages NuGet pour WebApi.Client et Newtonsoft.Json dans le projet.
    - Création de la classe Product (Id, Name, Price, Category).
    - Implémentation de RestApiQueries avec configuration HttpClient.
    - Refactoring de Program.cs avec une structure de base.

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpClientSample.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+    }
+}
---
+++ RestApiQueries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using HttpClientSample.Models;
+
+
+namespace HttpClientSample
+{
+    
+    public class RestApiQueries
+    {
+        private HttpClient _client;
+
+        public RestApiQueries()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
